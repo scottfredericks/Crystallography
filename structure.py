f97bcf75f169827ee2b631c75dbaf84c2041f73e
Scott <swf767@gmail.com>
2018-02-13 15:20:02 -0800

    Added conditionals for lattice choices.

--- structure.py
+++ structure.py
@@ -26,23 +26,48 @@ num_attempts2 = 10
 def choose_lattice(space_group_num, N):
 	sg = space_group_num
 	#Triclinic
-	if <= 2:
+	#P lattice
+	if sg <= 2:
 		pass 
 	#Monoclinic
 	elif sg <= 15:
-		pass
+		#C lattice
+		if sg in [5, 8, 9, 12, 15]:
+			pass
+		#P lattice
+		else:
+			pass
 	#Orthorhombic
 	elif sg <= 74:
-		pass
+		#C lattice
+		if sg in [20, 21, 35, 36, 37, 63, 64, 65, 66, 67, 68]:
+			pass
+		#F lattice
+		elif sg in [22, 42, 43, 69, 70]:
+			pass
+		#I lattice
+		elif sg in [23, 24, 44, 45, 46, 71, 72, 73, 74]:
+			pass
+		#A lattice
+		elif sg in [38, 39, 40, 41]:
+			pass
+		#P lattice
+		else:
+			pass
 	#Tetragonal
 	elif sg <= 142:
-		pass
+		#I lattice
+		if sg in [79, 80, 82, 87, 88, 97, 98, 107, 108, 109, 110, 119, 120, 121, 122, 139, 140, 141, 142]:
+			pass
+		#P lattice
+		else:
+			pass
 	#Trigonal/Rhombohedral
 	elif sg <= 167:
-		#Rhombohedral
+		#Rhombohedral (R) lattice
 		if sg in [146, 148, 155, 160, 161, 166, 167]:
 			pass
-		#Trigonal
+		#Trigonal (P) lattice
 		else:
 			pass
 	#Hexagonal
@@ -50,10 +75,21 @@ def choose_lattice(space_group_num, N):
 		pass
 	#Cubic
 	else:
-		pass
+		#F lattice
+		if sg in [196, 202, 203, 209, 210, 216, 219, 225, 226, 227, 228]:
+			pass
+		#I lattice
+		elif sg in [197, 199, 204, 206, 211, 214, 217, 220, 229, 230]:
+			pass
+		#P lattice
+		else:
+			s = N ** (1./3.)
+			a, b, c = s, s, s
+			alpha, beta, gamma = 90., 90., 90.
 
 #Create structure with pymatgen
-
+#primitive_lattice = pmg.core.lattice.from_lengths_and_angles()
+#pmg.core.structure.Structure.from_spacegroup(sg, lattice, species, coords)
 	#Add largest Wyckoff positions first (n<num_atoms)
 		#Check distance with periodic_site, merge if needed
 	#If n == num_atoms, success
