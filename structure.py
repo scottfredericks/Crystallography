284bf6636a7b0c74e39d6156729d79cfb52d98c2
Scott <swf767@gmail.com>
2018-02-23 19:15:25 -0800

    Fixed triclinic lattice conditions

--- structure.py
+++ structure.py
@@ -7,35 +7,45 @@ Outputs a cif file with conventional setting'''
 
 import pymatgen
 from random import uniform as rand
+from random import choice as choose
 from math import sqrt
 from math import pi
 from math import sin
 from math import cos
+from math import acos
+from math import fabs
 import database.make_sitesym as make_sitesym
 import database.hall as hall
 
 #Define variables
 #------------------------------
 deg = 2.*pi/360. #radian conversion factor for sin and cos
-min_separation = 1.0 #seperation tolerance in Angstroms
-max_attempts = 10
-'''space_group_num = int(input("International number of space group: "))
-if space_group_num < 1 or space_group_num > 230:
-	print("Error: Invalid space group number.")
-	quit()
-species = input("Atomic species (separated by commas): ").split(',')
-y = input("Number of atoms in the primitive cell (separated by commas): ").split(',')
-num_atoms = []
-for x in y:
-	num_atoms.append(int(x))
-if len(species) != len(num_atoms):
-	print("Error: Number of atoms does not match number of species.")
-	quit()'''
+tol = 1.0 #seperation tolerance in Angstroms
+max1 = 10 #Attempts for generating lattices
+max2 = 3 #Attempts for a given lattice
+max3 = 10 #Attempts for a given Wyckoff position
 
 #Define functions
 #------------------------------
+def displacement(xyz1, xyz2): #Displacement vector
+	return [xyz2[0]-xyz1[0], xyz2[1]-xyz1[1], xyz2[2]-xyz1[2]]
+
+def dsquared(xyz1, xyz2): #Distance squared
+	v = displacement(xyz1, xyz2)
+	return (v[0]**2 + v[1]**2 + v[2]**2)
+
+def distance(xyz1, xyz2): #Euclidean distance
+	return sqrt(dsquared(xyz1, xyz2))
+
 def rand_coords():
 	return [rand(0,1), rand(0,1), rand(0,1)]
+
+def filter_site(v):
+	w = v
+	for i in range(len(w)):
+		while w[i]<0: w[i] += 1
+		while w[i]>=1: w[i] -= 1
+	return w
 
 #Choose random lattice parameters consistent with the lattice type
 #Uses the conventional setting; pymatgen handles this with the Structure class
@@ -44,10 +54,14 @@ def choose_lattice(sg, N):
 	m = 2. #minimum lattice spacing
 	#Triclinic
 	if sg <= 2:
-		alpha = rand(30., 150.)
-		beta = rand(30., 150.)
 		gamma = rand(30., 150.)
-		x = sqrt(1. - cos(alpha*deg)**2 - cos(beta*deg)**2 - cos(gamma*deg)**2 + 2*cos(alpha*deg)*cos(beta*deg)*cos(gamma*deg))
+		beta = rand(30., 150.)
+		alpha = 0
+		while (alpha < 30. or alpha > 150.):
+			theta = rand(30., 150.) #alpha is restricted by beta and gamma, so an intermediary value is needed
+			if fabs(cos(gamma*deg)*cos(beta*deg)+sin(gamma*deg)*cos(theta*deg)) <= 1.0:
+				alpha = acos(cos(gamma*deg)*cos(beta*deg)+sin(gamma*deg)*cos(theta*deg)) / deg
+		x = sqrt(1. - cos(alpha*deg)**2 - cos(beta*deg)**2 - cos(gamma*deg)**2 + 2.*cos(alpha*deg)*cos(beta*deg)*cos(gamma*deg))
 		#P lattice
 		a = rand(2., v*N/(m*m*x))
 		b = rand(2., v*N/(a*m*x))
@@ -165,42 +179,80 @@ def connected_components(graph): #Return a set of connected components for an un
 		i += 1
 	return sets
 
-#Main Program Loop
-#------------------------------
-'''def generate_structure(space_group_num, species, num_atoms, max_attempts)
-	y = 0
+def generate_structure(space_group_num, species, num_atoms):
+	wyckoffs = get_wyckoff_positions(space_group_num) #2d array of Symm_Ops
+	i = 0
+	j = 0
+	wyckoffs_organized = [[]] #2D Array of Wyckoff positions organized by multiplicity
+	old = len(wyckoffs[0])
+	for x in wyckoffs:
+		mult = len(wyckoffs[i])
+		if mult != old:
+			i += 1
+			old = mult
+		wyckoffs_organized[i].append(x)
+	y = 0 #total number of atoms in primitive cell
 	for x in num_atoms:
 		y += x
-	loop1 = True
-	num_attempts = 0
-	while(loop = True and num_attempts < max_attempts):
-		params = choose_lattice(space_group_num, y)
-		conventional_lattice = pymatgen.core.lattice.Lattice.from_lengths_and_angles( params[0], params[1] )
-		current_atoms = 0 #number of atoms successfully added
-		while(loop2 = True and current_atoms < y)
-	
-	
-		num_attempts += 1
-'''
+	attempts1 = 0 #Num of attempted lattices
+	success = False #True once successful structure has been generated
+	while (attempts1 < max1 and not success):
+		#Generate a lattice and empty structure object
+		lat = choose_lattice(space_group_num, y)
+		conventional_lattice = pymatgen.core.lattice.Lattice.from_lengths_and_angles(lat[0], lat[1])
+		attempts2 = 0 #Num of attempts for a given lattice
+		while (attempts2 < max2 and not success):
+			struct1 = pymatgen.core.structure.Structure.from_spacegroup(space_group_num, conventional_lattice, [], [])
+			#Add atoms one species at a time
+			i = 0
+			success_atom = True
+			while (i < len(species) and success_atom):
+				attempts3 = 0 #Num of attempts for a given species
+				#Strangely, just using struct2 = struct1 causes methods on struct2 to affect struct1
+				success_atom = False
+				element = species[i]
+				needed = num_atoms[i]
+				total = 0
+				while (not success_atom and attempts3 < max3):
+					loop = True
+					j = 0
+					while loop:
+						mult = len(wyckoffs_organized[j])
+						if (needed - total <= mult):
+							#Choose a random Wyckoff position for given multiplicity
+							ops = choose(wyckoffs_organized[j])
+							#Generate a list of coords from ops
+							point = rand_coords()
+							new_coords = []
+							for x in ops:
+								new_coords.append(x.operate(point))
+							#Create a graph from new_coords
+							graph = []
+							struct2 = pymatgen.core.structure.Structure.from_spacegroup(1, conventional_lattice, ['H'], new_coords)
+							for x in struct2.sites:
+								pass
+							loop = False
+						else:
+							j += 1
+							if j >= len(wyckoffs_organized): loop = False
+					attempts3 += 1
+				i += 1
+			if not success_atom: attempts2 += 1
+		if not success: attempts1 += 1
+	return struct1
+
 #-----------------Test Functionality------------------
-#struct1 = pymatgen.core.structure.Structure.from_spacegroup(space_group_num, conventional_lattice, [species[0]], [rand_coords()])
-	#Add largest Wyckoff positions first (n<num_atoms)
-		#Check distance with periodic_site, merge if needed
-	#If n == num_atoms, success
-	#else, try again up to num_attempts_2 times
-	#If still failed, start over up to num_attempts_1 times
-#If unsuccessful, output error message
-#If successful, output Cif file (convert from primitive cell if needed)
-mygraph =[
-[1],
-[0,2],
-[1],
-[4,5],
-[3,5],
-[3,4],
-[],
-[8,9],
-[7,9],
-[7,8,10],
-[9]]
-print(connected_components(mygraph))
+'''space_group_num = int(input("International number of space group: "))
+if space_group_num < 1 or space_group_num > 230:
+	print("Error: Invalid space group number.")
+	quit()
+species = input("Atomic species (separated by commas): ").split(',')
+y = input("Number of atoms in the primitive cell (separated by commas): ").split(',')
+num_atoms = []
+for x in y:
+	num_atoms.append(int(x))
+if len(species) != len(num_atoms):
+	print("Error: Number of atoms does not match number of species.")
+	quit()'''
+print(generate_structure(1, ['H'], [1]))
+
